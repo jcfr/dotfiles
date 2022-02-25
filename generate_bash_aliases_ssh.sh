5b404e3284cd1aeb9b2a2c255ecf30ffad783262
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-02-25 06:32:00 -0500

    style: generate_bash_aliases_ssh: Fix SC2002, SC2086, SC2019, SC2018
    SC2002: Useless cat. Consider 'cmd < file | ..' or 'cmd file | ..' instead
    SC2086: Double quote to prevent globbing and word splitting.
    SC2019: Use '[:upper:]' to support accents and foreign alphabets.
    SC2018: Use '[:lower:]' to support accents and foreign alphabets.

--- generate_bash_aliases_ssh.sh
+++ generate_bash_aliases_ssh.sh
@@ -8,9 +8,9 @@ echo -n "" > ${output}
 
 pushd ~/.remmina > /dev/null
 for conf_file in $(ack --type-add=remmina:ext:remmina --remmina protocol=SSH -l); do
-  server=$(cat ${conf_file} | ack "^server=" | cut -d= -f2)
-  ssh_username=$(cat ${conf_file} | ack "^ssh_username=" | cut -d= -f2)
-  name=$(cat ${conf_file} | ack "^name=" | cut -d= -f2)
+  server=$(ack "^server=" "${conf_file}" | cut -d= -f2)
+  ssh_username=$(ack "^ssh_username=" "${conf_file}" | cut -d= -f2)
+  name=$(ack "^name=" "${conf_file}" | cut -d= -f2)
 
   # Copied from https://stackoverflow.com/a/94500/1539918
   # first, strip underscores
@@ -20,11 +20,11 @@ for conf_file in $(ack --type-add=remmina:ext:remmina --remmina protocol=SSH -l)
   # now, clean out anything that's not alphanumeric or an underscore
   name=${name//[^a-zA-Z0-9_]/}
   # finally, lowercase with TR
-  name=$(echo -n $name | tr A-Z a-z)
+  name=$(echo -n "$name" | tr "[:upper:]" "[:lower:]")
 
   alias_cmd="alias ssh-connect-${name}='ssh ${ssh_username}@${server}'"
   echo "[${output}] ${alias_cmd}"
-  echo ${alias_cmd} >> ${output}
+  echo "${alias_cmd}" >> "${output}"
 done
 
 popd > /dev/null
