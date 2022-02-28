46c99c7efaf3d3cad77a210981928c55f0e3211e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-02-28 08:01:04 -0500

    dockerfunc/functions/test.sh/ninja completion: Convert tabs to 2 spaces
    ```
    cd dotfiles
    for file in .dockerfunc .functions test.sh etc/bash_completion.d/ninja; do
      expand_tabs_as_two_spaces $file
    done
    ```
    
    See https://unix.stackexchange.com/questions/39210/whats-the-standard-for-indentation-in-shell-scripts

--- test.sh
+++ test.sh
@@ -6,19 +6,19 @@ ERRORS=()
 
 # find all executables and run `shellcheck`
 for f in $(find . -type f -not -path '*.git*' -not -name "aptfile" | sort -u); do
-	if file "$f" | grep --quiet shell; then
-		{
-			shellcheck "$f" && echo "[OK]: successfully linted $f"
-		} || {
-			# add to errors
-			ERRORS+=("$f")
-		}
-	fi
+  if file "$f" | grep --quiet shell; then
+    {
+      shellcheck "$f" && echo "[OK]: successfully linted $f"
+    } || {
+      # add to errors
+      ERRORS+=("$f")
+    }
+  fi
 done
 
 if [ ${#ERRORS[@]} -eq 0 ]; then
-	echo "No errors, hooray"
+  echo "No errors, hooray"
 else
-	echo "These files failed shellcheck: ${ERRORS[*]}"
-	exit 1
+  echo "These files failed shellcheck: ${ERRORS[*]}"
+  exit 1
 fi
